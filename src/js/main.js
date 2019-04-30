387debcb7f310b0bc58039ff0bfea990520338dd
jb.lindner <jb.lindner@orange.fr>
2019-04-30 09:10:52 +0200

    add fonctionnel search

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,37 @@
+$(document).ready(function(){
+      // GET DOM
+      let search = $('#search');
+      let searchInput = $('#search-input');
 
-$.ajax({
-    url: 'https://api.deezer.com/search?q=eminem&output=jsonp',
-    dataType: 'jsonp'
-}).done(function(musiques){
+      search.on('submit', function(e){
+          e.preventDefault();
 
-    document.querySelector('#results').innerHTML = 
-        musiques.data.map(m => m.title).join('<br>');
+          // Remove all cards
+          $('.card').remove();
 
+          // Récupération valeur input
+          let searchValue = searchInput.val();
+
+          //Requête ajax api
+          $.ajax({
+                url: 'https://api.deezer.com/search?q='+ searchValue +'&output=jsonp',
+                dataType: 'jsonp'
+          }).done(function(musiques){
+                let musiqueSearch = musiques.data
+                
+                // Boucle affichant les cards
+                for (let i = 0; i < musiqueSearch.length; i++) {
+
+                    let titleValue = musiqueSearch[i].title;
+                    let artisteValue = musiqueSearch[i].artist.name;
+                    let albumValue = musiqueSearch[i].album.title;
+                    let coverSrc = musiqueSearch[i].album.cover;
+
+                    // Insertion code html
+                    $('.grid-cards').append('<div class="card"><div class="image" style="background-image: url(\''+ coverSrc +'\')"></div><h4 class="title title-card">'+ titleValue +'</h4><span class="subtitle">'+ artisteValue +' - '+ albumValue +'</span><ion-icon name="heart-empty" class="fav"></ion-icon></div>');
+                    
+                }
+          });
+      });
+      
 });
---
+++ src/js/pages/search.js
@@ -0,0 +1,28 @@
+
+export default class Search {
+    // // GET DOM
+    // let searchInput = document.querySelector('#search-input');
+    // let button = document.querySelector('#search-button');
+    // let searchValue = searchInput.val();
+
+    // button.addEventListener('click', function(){
+    //     document.querySelector('.card').innerHTML = searchValue;
+
+    // });
+    // $.ajax({
+    //         url: 'https://api.deezer.com/search?q=eminem&output=jsonp',
+    //         dataType: 'jsonp'
+    //     }).done(function(musiques){
+
+    // // document.querySelector('.card').innerHTML = 
+    // //     musiques.data.map(m => m.id).join('<br>');
+
+    // });
+    constructor() {
+        this.view = 'search.html';
+    }
+    
+    init() {
+        alert('Welcome meteo');       
+    }
+}
